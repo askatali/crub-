572bbcb8512e751e59915c8dea37f77965e4009e
baha_2002 <baktiarzaksylykov22@gmail.com>
2023-12-26 17:20:28 +0600

    first commit

---
+++ product/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='описание')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+    size = models.IntegerField(default=38, verbose_name='Размер')
+    color = models.CharField(max_length=256, verbose_name='Цвет')
+    image = models.ImageField(upload_to='product_images', verbose_name='Фото')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.title}'
