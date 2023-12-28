a6e9cbaeea9f2651e9a0a0b963ad5908e635f7af
askatali <isenbekovsanat8@gmail.com>
2023-12-28 15:09:11 +0300

    first commit

--- product/views.py
+++ product/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from product.models import Product
 
 
-def index(request):
-    return HttpResponse('<h1>Hello World!<h1/>')
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 
+                  'index.html',
+                  context={'products':products}
+                  )
+ 
