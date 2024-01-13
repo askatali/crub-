1af56663e782050936b2657e02a173fe80a3cad4
baha_2002 <baktiarzaksylykov22@gmail.com>
2024-01-13 17:12:44 +0600

    templateview

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from product.views import product_list, product_detail
+from product.views import ProductListView, product_detail
 
 urlpatterns = [
-    path('list/', product_list, name='list'),
+    path('list/', ProductListView.as_view(), name='list'),
     path('detail/<int:pk>/', product_detail, name='detail')
 ]
--- product/views.py
+++ product/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from product.models import Product
+from django.views.generic import TemplateView
 
 
-def product_list(request):
-    products = Product.objects.order_by('-size')
-    return render(request,
-                  'index.html',
-                  context={'products': products}
-                  )
+class ProductListView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        products = Product.objects.order_by('-size')
+        return {'products': products}
 
 
 def product_detail(request, pk):
